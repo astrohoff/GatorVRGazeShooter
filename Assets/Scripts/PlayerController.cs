44f168455d57cc7222c0ad49aefde4e9215c9c62
astrohoff <astrohoff@ufl.edu>
2016-10-04 08:51:04 -0400

    Created a PlayerController script that spawns a projectile when the shoot button is pressed. Created a ProjectileController script that gives the projectile a forward velocity when it is created.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This class is for player behaviors. 
+public class PlayerController : MonoBehaviour
+{
+    public GvrViewer gvrViewer;
+    public GameObject projectilePrefab;
+
+    // Use this for initialization.
+
+    // Update is called once per frame.
+    void Update()
+    {
+        // If the Cardboard trigger is pressed (also if screen is touched, and simulated by mouse click in Unity editor),
+        // spawn a new projectile at the camera's position.
+        // Projectile is also given the camera's rotation so that it can just move forward once it has been created.
+        if(gvrViewer.Triggered)
+        {
+            Instantiate(projectilePrefab, transform.position, transform.rotation);
+        }
+    }
+}
